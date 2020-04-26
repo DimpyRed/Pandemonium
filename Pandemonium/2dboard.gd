e4411fae9f66066958516f3f3242e738d7152d87
cory <quinlanc@sonoma.edu>
2020-04-25 18:08:55 -0700

    removed clutter, renamed where it made sense

--- Pandemonium/2dboard.gd
+++ Pandemonium/2dboard.gd
@@ -6,4 +6,4 @@ func _ready():
 
 
 func _on_touch_pressed():
-	pass # Replace with function body.
+	pass
--- Pandemonium/movepiece.gd
+++
@@ -1,7 +0,0 @@
-extends TextureButton
-
-func get_drag_data(_pos):
-	var piece = TextureButton.new()
-	set_drag_preview(piece)
-
-
